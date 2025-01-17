b0e5263c50eec28102f42493011dde46498a7d5e
Miaki Yamazaki <atarashi1216@gmail.com>
2025-01-17 16:38:02 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import time
+
+
+st.title('streamlit 超入門')
+
+st.write('プレグレスバーの表示')
+'Start!!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration{i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!!'
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラムです。')
+
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+
+
+
+# text = st.text_input('あなたの趣味を教えてください。')
+# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+
+# 'あなたの趣味：', text
+# 'コンディション：', condition
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('image_123650291.JPG')
+#     st.image(img, caption='miaki', use_container_width=True)
+
